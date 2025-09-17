99d21f9fde09a5fb0b89a6040e5742200df40360
tulkin07 <akhmadov0770@gmail.com>
2025-09-17 08:44:33 -0400

    remove config url

--- src/app/siteConfig.ts
+++ src/app/siteConfig.ts
@@ -2,13 +2,6 @@ export const siteConfig = {
   name: "Lx1",
   url: "https://lxone.vercel.app",
   description: "Lx1 - Transporation Management System",
-  // baseLinks: {
-  //   quotes: {
-  //     overview: "/dashboard",
-  //     monitoring: "/",
-  //     audits: "/",
-  //   },
-  // },
 }
 
 export type siteConfig = typeof siteConfig
