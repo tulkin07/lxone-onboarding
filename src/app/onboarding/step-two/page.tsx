35b5ce30060d8bb58ce482ac94cb731dc89198e2
tulkin07 <akhmadov0770@gmail.com>
2025-09-17 07:41:32 -0400

    first commit

--- src/app/onboarding/step-two/page.tsx
+++ src/app/onboarding/step-two/page.tsx
@@ -183,7 +183,6 @@ export default function Products() {
           </div>
           <div>
             <Label
-              
               className="mb-1 block text-sm font-medium text-gray-700"
             >
                 &nbsp;
